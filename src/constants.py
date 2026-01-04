791709437b85f19b734375785b56c4ca4d5a8185
Jesús Soto <jesus.soto@pucp.edu.pe>
2026-01-04 11:01:37 -0500

    constatnts

--- src/constants.py
+++ src/constants.py
@@ -8,6 +8,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 gcp_secret = os.getenv("GCP_SECRET")
+project_id = os.getenv("PROJECT_ID")
+
+print(f"DEBUG: raw GCP_SECRET='{gcp_secret}', PROJECT_ID='{project_id}'")
+
+if gcp_secret and not gcp_secret.startswith("projects/"):
+    if project_id:
+        print("DEBUG: constructing full path from short name and PROJECT_ID")
+        gcp_secret = f"projects/{project_id}/secrets/{gcp_secret}/versions/latest"
+    else:
+        print("WARNING: GCP_SECRET looks like a short name but PROJECT_ID is missing!")
+
+print(f"DEBUG: final GCP_SECRET='{gcp_secret}'")
+
 client = secretmanager.SecretManagerServiceClient()
 response = client.access_secret_version(name=gcp_secret)
 my_secret_value = response.payload.data.decode("UTF-8")
